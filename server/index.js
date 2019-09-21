bab59b1f227d2205f626adb158cae98eade5132e
SaadBenn <saadbennmushtaq@gmail.com>
2019-09-21 14:56:26 -0400

    Stub out db for testing

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 const express = require('express')
+import db from './stubDb/db';
 
+// set up the express app
 const app = express() 
 const port = 3000
 
 app.get('/', (req, res) => res.send('6ix In The City'))
 
-app.listen(port, () => console.log('Server running on port 3000'))
+app.get('/bill', (req, res) => {
+	res.status(200).send({
+		success: 'true',
+		message: 'got the bill',
+		bill: db
+	})
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
