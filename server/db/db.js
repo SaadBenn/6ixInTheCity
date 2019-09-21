8800d32ec5f5132d09ff55f4660fb71df97aaa77
SaadBenn <saadbennmushtaq@gmail.com>
2019-09-21 16:27:32 -0400

    Add morgan and passport

---
+++ server/db/db.js
@@ -0,0 +1,12 @@
+const bill = [
+	{
+		customer_id: 1,
+		food: {
+			"pizza": $10,
+			"OG burger": $1,
+			"poison": $0
+		}
+	}
+];
+
+export default bill;
--- server/index.js
+++ server/index.js
@@ -1,18 +1,31 @@
-const express = require('express')
-import db from './stubDb/db';
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const morgan = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const errorHandler = require('errorhandler');
+
+// Configure mongoose's promise to global promise
+mongoose.promise = global.Promise;
+
+const port = process.env.PORT || 2000;
 
 // set up the express app
-const app = express() 
-const port = 3000
+const app = express(); 
 
-app.get('/', (req, res) => res.send('6ix In The City'))
-
-app.get('/bill', (req, res) => {
+// middlewares are run in sequence
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+// pasrsing the json object
+app.use(bodyParser.json());
+app.get('/', (req, res) => {
 	res.status(200).send({
-		success: 'true',
-		message: 'got the bill',
-		bill: db
+		success: 'true'
 	})
 });
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+var server = app.listen(port, () => console.log(`Server running on port ${port}`));
+
+module.exports = server;
