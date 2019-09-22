697853f91fefcc58ff73b23bff0236654bf666c0
SaadBenn <saadbennmushtaq@gmail.com>
2019-09-22 11:05:29 -0400

    ADd post request for adding items

--- server/routes/meals.js
+++ server/routes/meals.js
@@ -6,9 +6,13 @@ const mealsController = require('../controllers/meals')
 
 console.log('IN routes');
 
-router.route('')
+router.route('/menu')
 	.get(validateBody(mealSchema.authSchema), mealsController.meals);
 
-console.log('After routes');
+console.log("Before add call");
+router.route('/add')
+    .post(validateBody(mealSchema.authSchema), mealsController.add);
+
+console.log('After add call');
 
 module.exports = router;
